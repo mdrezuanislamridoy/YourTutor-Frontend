8e2d4fc29e01f9da9b850e0b21b70ecd0dffbea8
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-10-28 00:53:52 +0600

    fixed dotnev

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:9999/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
+
+console.log(import.meta.env.VITE_BACKEND_URL);
 
 export default axiosInstance;
