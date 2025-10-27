77a4e9d66f58181ee06f984806a2a8d554966f1a
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-10-28 00:28:41 +0600

    i donno

--- src/pages/profiles/admin/components/AddCourseModal.tsx
+++ src/pages/profiles/admin/components/AddCourseModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import axiosInstance from "../../../../lib/axiosInstance";
 
