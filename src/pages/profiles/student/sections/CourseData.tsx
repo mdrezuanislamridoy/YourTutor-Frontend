8acda5ac15329c038c6bd4a8fef9d84aaf16a04c
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-10-30 18:43:40 +0600

    mistake by vul

--- src/pages/profiles/student/sections/CourseData.tsx
+++ src/pages/profiles/student/sections/CourseData.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../../../lib/axiosInstance";
 import { Loader2, ChevronDown, ChevronUp, PlayCircle } from "lucide-react";
