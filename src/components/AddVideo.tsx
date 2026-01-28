f6d73b8a1204da0a7e110391e82c3d1cc2041b44
MD. Ridoy Babu <147362353+mdrezuanislamridoy@users.noreply.github.com>
2026-01-29 02:38:17 +0600

    Comment out courses state and fetchCourse function
    Comment out the courses state and fetchCourse function.

--- src/components/AddVideo.tsx
+++ src/components/AddVideo.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import axiosInstance from "../lib/axiosInstance";
 
 export default function AddVideo() {
-  const [courses, setCourses] = useState([]);
+  // const [courses, setCourses] = useState([]);
 
-  const fetchCourse = async () => {
-    const res = await axiosInstance.get("/course/get-courses");
-    setCourses(res?.data?.courses || []);
-  };
+  // const fetchCourse = async () => {
+  //   const res = await axiosInstance.get("/course/get-courses");
+  //   setCourses(res?.data?.courses || []);
+  // };
   
 
   return <div>AddVideo</div>;
